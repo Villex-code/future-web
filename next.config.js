a46bd780493a3491107adcb0becba36e8c465014
Villex <videlik@gmail.com>
2023-01-29 21:57:04 +0100

    original working

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  output: "standalone",
-  experimental: {
-    // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
 };
